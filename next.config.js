f45baff8cadd8860f8a209e519973969d6355812
meditationastro <meditationastroweb@gmail.com>
2026-02-24 03:00:35 +0545

    g

--- next.config.js
+++ next.config.js
@@ -3,8 +3,6 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'source.unsplash.com' },
-      { protocol: 'https', hostname: 'images.pexels.com' },
       { protocol: 'https', hostname: 'res.cloudinary.com' },
     ],
   },
